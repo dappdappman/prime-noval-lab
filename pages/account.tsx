14d3384d257802881c7bfa88f4dcb8f5e321b7fd
David Bilson <davidbilson99@gmail.com>
2024-12-16 09:23:11 +0100

    updated account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,10 +1,7 @@
-import dynamic from "next/dynamic";
 import Link from "next/link";
 import styles from "@/styles/account.module.css";
-import animationData from "@/public/loading.json";
 import Head from "next/head";
-
-const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+import BounceLoader from "react-spinners/BounceLoader";
 
 const Account = () => {
   return (
@@ -16,16 +13,8 @@ const Account = () => {
       <h1 className={styles.errorHeading}>Loading Account</h1>
 
       <div className={styles.animationContainer}>
-        <Lottie
-          animationData={animationData}
-          loop={true}
-          autoplay={true}
-          className={styles.animation}
-          aria-label="Loading animation"
-          />
+        <BounceLoader color="#00b12c" />
       </div>
-
-      <h1 className={styles.errorSubheading}>Loading Account...</h1>
 
       <p className={styles.errorMessage}>
         If it takes too long, kindly restart the process as your credentials may
